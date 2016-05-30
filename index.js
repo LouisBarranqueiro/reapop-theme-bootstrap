e0133e61dd60c7777608f38aa1fc0c0677fbe165
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-05-30 23:24:17 +0200

    update transition time out

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ var notificationsContainerClassName = {
 
 // default transition for Notification component
 var notificationsContainerTransition = {
-  appearTimeout: 1000,
-  enterTimeout: 1000,
-  leaveTimeout: 1000,
+  appearTimeout: 450,
+  enterTimeout: 450,
+  leaveTimeout: 450,
   name: {
     appear: css['notification-appear'],
     appearActive: css['notification-appear-active'],
